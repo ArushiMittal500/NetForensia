422ca03e8cdc30eda95a4b5bbec819f6eb27be87
Drstrange2903 <73109069+Drstrange2903@users.noreply.github.com>
2022-11-30 11:19:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
-import pyshark
+import os
+
 
 class test(unittest.TestCase):
     def __init__(self,iface_name,filter_string,interface,bpf_filter):
@@ -8,24 +9,15 @@ class test(unittest.TestCase):
         self.interface=interface
         self.bpf_filter=bpf_filter
         pass
-    def test_iface_name(self,iface_name):
-        self.assertEqual(iface_name,'Ethernet')
-    def test_filter_string(self,filter_string):
-        self.assertEqual(filter_string,'port 443')       
-    def capture(self,interface,bpf_filter):
+         
+    def capture(self,interface,bpf_filter,iface_name,filter_string):
         self.assertNotEqual(interface,'')
-        self.assertNotEqual(bpf_filter,'')       
-    
-    
-    
-    
-    iface_name = 'Ethernet'
-    filter_string = 'port 443'
-    capture = pyshark.LiveCapture(
-        interface=iface_name,
-        bpf_filter=filter_string,
-        output_file="packet.pcap")
-    capture.sniff(timeout=5, packet_count=50)
-    if len(capture) > 0:
-        for packet in capture:
-            print('source: ' + packet.ip.src + " Destination: " + packet.ip.dst)
+        self.assertNotEqual(bpf_filter,'')
+        self.assertEqual(iface_name,'Ethernet')
+        self.assertEqual(filter_string,'port 443')       
+    def capturesniff(self,timeout,packet_count):
+        self.assertNotEqual(timeout,0)
+        self.assertCountEqual(packet_count,50)
+    def generted(self,path):
+        print(os.path.isfile(path))
+  
