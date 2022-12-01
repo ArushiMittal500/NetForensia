2317a03447ba697ea7ffeaecdb9a7a8ff1ab8d11
Drstrange2903 <73109069+Drstrange2903@users.noreply.github.com>
2022-12-01 15:47:46 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,23 +1,16 @@
-import unittest
+from capture import sniff_packets
 import os
 
 
-class test(unittest.TestCase):
-    def __init__(self,iface_name,filter_string,interface,bpf_filter):
-        self.iface_name=iface_name
-        self.filter_string=filter_string
-        self.interface=interface
-        self.bpf_filter=bpf_filter
-        pass
-         
-    def capture(self,interface,bpf_filter,iface_name,filter_string):
-        self.assertNotEqual(interface,'')
-        self.assertNotEqual(bpf_filter,'')
-        self.assertEqual(iface_name,'Ethernet')
-        self.assertEqual(filter_string,'port 443')       
-    def capturesniff(self,timeout,packet_count):
-        self.assertNotEqual(timeout,0)
-        self.assertCountEqual(packet_count,50)
-    def generted(self,path):
-        print(os.path.isfile(path))
-  
+def test_sniff_packets():
+    iface_name = 'wlp0s20f3'
+    filter_string = 'port 443'
+    output_file = "test_capturepackets.pcap"
+
+    number_of_captures = sniff_packets(iface_name, filter_string, output_file)
+
+    assert number_of_captures > 0
+    assert (iface_name,'Ethernet')
+    assert (filter_string,'port 443')
+    assert os.path.exists(output_file) == True
+    
