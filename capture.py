fbbe1c18141ca8a1344dec671c1d6975d5933668
ArushiMittal500 <80587194+ArushiMittal500@users.noreply.github.com>
2022-12-02 15:38:44 +0530

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,12 +1,24 @@
 import pyshark
-iface_name = 'Ethernet'
-filter_string = 'port 443'
-capture = pyshark.LiveCapture(
-    interface=iface_name,
-    bpf_filter=filter_string,
+
+def sniff_packets(iface_name, filter_string, output_file):
+    capture = pyshark.LiveCapture(
+        interface=iface_name,
+        bpf_filter=filter_string,
+        output_file=output_file
+    )
+    capture.sniff(timeout=5, packet_count=50)
+    print(len(capture))
+    if len(capture) > 0:
+        for index, packet in enumerate(capture):
+            if "ip" in packet:
+                print(f'{index} source: {packet.ip.src} Destination:  {packet.ip.dst}')
+
+    return len(capture)
+
+
+
+if __name__ == "__main__":
+    iface_name = 'wlp0s20f3'
+    filter_string = 'port 443'
     output_file="capturepackets.pcap"
-)
-capture.sniff(timeout=5, packet_count=50)
-if len(capture) > 0:
-    for packet in capture:
-        print('source: ' + packet.ip.src + " Destination: " + packet.ip.dst)
+    sniff_packets(iface_name, filter_string, output_file)
